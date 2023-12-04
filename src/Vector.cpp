6b155d86aed0172e7c38c0f07a6439b41696624b
Andy Bakir <107201914+AndyBakirCS@users.noreply.github.com>
2023-12-03 22:41:53 -0800

    Update Vector.cpp

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -4,14 +4,14 @@
 
 #include "gtest/gtest.h"
 
-(vector_v_vector_pair_e, euler_walkable1) {
+TEST(vector_v_vector_pair_e, euler_walkable1) {
   std::vector<int> v = {0, 1, 2, 3};
   std::vector<std::pair<int, int>> e = {{0, 1}, {1, 3}, {3, 2}, {2, 0}};
   Graph g(v, e);
   EXPECT_EQ(true, g.IsEulerWalkable());
 }
 
-(vector_v_vector_pair_e, euler_walkable2) {
+TEST(vector_v_vector_pair_e, euler_walkable2) {
   std::vector<int> v = {0, 1, 2, 3};
   std::vector<std::pair<int, int>> e = {{0, 1}, {0, 2}, {0, 3},
                                         {1, 2}, {1, 3}, {2, 3}};
@@ -19,7 +19,7 @@
   EXPECT_EQ(false, g.IsEulerWalkable());
 }
 
-(vector_v_vector_pair_e, euler_walkable3) {
+TEST(vector_v_vector_pair_e, euler_walkable3) {
   std::vector<int> v = {0, 1, 2, 3};
   std::vector<std::pair<int, int>> e = {{1, 3}, {1, 3}, {3, 0}, {3, 0},
                                         {0, 2}, {2, 1}, {2, 3}};
